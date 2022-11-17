6a8157af4b6699c66ffc180a48e0c026aa40a40a
Edson Sales <edsonsales99@gmail.com>
2022-11-17 10:59:26 -0400

    Adicioando ordenação pa pagina home

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,19 +1,19 @@
 package br.com.alura.mvc.mudi.repository;
 
-import java.util.List;
-
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import br.com.alura.mvc.mudi.model.Pedido;
-import br.com.alura.mvc.mudi.model.StatusPedido;
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-	List<Pedido> findByStatus(StatusPedido aguardando);
+    List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("select p from Pedido p join p.user u where u.username = :username")
     List<Pedido> findAllByUsusario(@Param("username") String username);
